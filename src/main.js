fcf4caf3fe9593119651eb9bd72280665fdbe59a
Benjapol Worakan <benwrk@live.com>
2018-08-15 06:31:38 +0700

    Add Bootstrap and jQuery via `main.js` instead

--- src/main.js
+++ src/main.js
@@ -3,19 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'font-awesome/css/font-awesome.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VeeValidate from 'vee-validate'
 import Vuetable from 'vuetable-2'
+// import jQuery from 'jquery'
 
 Vue.use(Vuetable)
 Vue.use(VeeValidate)
 Vue.use(VueAxios, axios)
-Vue.use(BootstrapVue)
+// Vue.use(jQuery)
+// Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
