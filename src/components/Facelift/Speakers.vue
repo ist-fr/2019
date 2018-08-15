e1a57ff3733ac6176fd43e551014f6c40d18414e
Norawit Urailertprasert <norawit543@hotmail.com>
2018-08-15 14:21:28 +0700

    fix logo

--- src/components/Facelift/Speakers.vue
+++ src/components/Facelift/Speakers.vue
@@ -29,14 +29,27 @@ export default {
       speakers: [
         {
           name: 'Dusit Niyato',
-          info: 'IEEE Fellow, citation: 19755, h-index: 67,  150+ publications, 5 books, 190+ conference, Nanyang Technological University, Singapore',
+          info: 'IEEE Fellow, citation: 19755, h-index: 67,  150+ publications, 5 books, 190+ conference, ',
           topic: 'TBA',
+          from: 'Nanyang Technological University, Singapore',
           image: 'http://www.ntu.edu.sg/home/dniyato/tao2018.jpg',
           link: 'http://www.ntu.edu.sg/home/dniyato/'
         },
         {
           name: 'Supasorn Suwajanakorn',
-          info: 'former research resident @ Google Brain. Synthesizing Obama: Learning Lip Sync from Audio'
+          info: 'former research resident @ Google Brain. Synthesizing Obama: Learning Lip Sync from Audio',
+          topic: 'TBA',
+          from: 'VISTEC',
+          image: 'https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/38512095_10160607851845526_6187919302159499264_n.jpg?_nc_cat=0&_nc_eui2=AeHrzh2XfFmEVkDPeaBpbMC-zDnBmdQnYo52bdZXLYgxXfyAk2TU6_NTQfgouY6kUNu9RuQen8Ls6LRAwmA00mYgL5sgMPXaVSoqsF-8mBDAbw&oh=de5eeac8a1ea85fff5d8e4763a44891e&oe=5BC58F35',
+          link: 'http://www.supasorn.com/'
+        },
+        {
+          name: 'Thanawin Rakthanmanon',
+          info: '',
+          topic: '',
+          from: 'Kasetsart',
+          image: '',
+          linl: ''
         }
       ]
     }
