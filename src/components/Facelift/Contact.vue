2b77883296558c2666e702e343a3ffee52ddfec0
Benjapol Worakan <benwrk@live.com>
2018-08-15 08:13:58 +0700

    Add contacts

--- src/components/Facelift/Contact.vue
+++ src/components/Facelift/Contact.vue
@@ -5,8 +5,47 @@
         <div class="col-lg-12 mx-auto">
           <h2>Contact</h2>
           <p class="lead">Feel free to reach us</p>
+          <div class="mt-4 flex-center">
+            <a class="social" href="https://www.facebook.com/Vidyasirimedhi">
+              <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            <!-- <a class="tw-ic">
+              <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a> -->
+            <!-- <a class="gplus-ic">
+              <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i> -->
+            <!-- </a> -->
+            <!-- <a class="li-ic">
+              <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a> -->
+            <!-- <a class="ins-ic">
+              <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a> -->
+            <!-- <a class="pin-ic">
+              <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+            </a> -->
+            <a class="social" href="https://www.youtube.com/channel/UCyiO-CBG4J__S83jzq3k0pQ">
+              <i class="fa fa-youtube fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            <a class="social" href="https://www.vistec.ac.th">
+              <i class="fa fa-globe fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            <a class="social" href="tel:+6633014444">
+              <i class="fa fa-phone fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+            <a class="social" href="mailto:contact@vistec.ac.th">
+              <i class="fa fa-at fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<style>
+  .social {
+    color: #212529;
+  }
+</style>
+
