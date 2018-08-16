f27655ff2aeb3d6a726dc880aab0721d61f022f5
Benjapol Worakan <benwrk@live.com>
2018-08-17 06:13:31 +0700

    Update layout and data model for committee

--- src/components/Facelift/Committee.vue
+++ src/components/Facelift/Committee.vue
@@ -4,19 +4,18 @@
       <div class="row">
         <div class="col-lg-12 mx-auto">
           <h2>COMMITTEE</h2>
-          <p class="lead"></p>
-
-          <div v-for="commitee in commitees" :key="commitee.name" class="card">
-          <a href="http://www.vistec.ac.th/academic/faculty_detail.php?school=IST&id=55">
-            <img :src="commitee.image" height="250px">
-          </a>
-          <h2 class="card-title">{{commitee.name}}</h2>
-          <div class="card-body"> 
-            <p class="card-text">
-              Incremental Query Processing<br>Large-scale Scientific Databases<br>Distributed Systems
-            </p>
-          </div>
         </div>
+        <div class="col-lg-6 col-sm-12 text-center card-committee" v-for="committee in committees" v-bind:key="committee.name">
+            <div class="img">
+              <img class="img-committee" :src="committee.imageSrc">
+            </div>
+            <div>
+              <small>{{committee.title}}</small>
+              <h3>{{committee.name}}</h3>
+              <h6>{{committee.position}}</h6>
+              <h6>{{committee.school}}</h6>
+              <h6>{{committee.university}}</h6>
+            </div>
         </div>
       </div>
     </div>
@@ -26,18 +25,50 @@
 export default {
   data () {
     return {
-      commitees: [
+      committees: [
         {
-          name: 'Assoc. Prof. Dr. Sarana Nutanong',
-          image: 'http://www.vistec.ac.th/uploads/staff/e1fad935c6752ccd81949fce3d7035e1.png',
-          website: ''
+          title: 'Assoc. Prof. Dr.',
+          name: 'Sarana Nutanong',
+          imageSrc: require('../../assets/committee/dr_sarana_nutanong.png'),
+          position: 'Faculty Member',
+          school: 'School of Information Science & Technology',
+          university: 'Vidyasirimedhi Institute of Science & Technology',
+          website: 'http://www.vistec.ac.th/academic/faculty_detail.php?school=IST&id=55'
         }
       ]
     }
-  },
-  components: {
-  },
-  method: {
   }
 }
 </script>
+
+<style scoped>
+  .img {
+    border-radius: 100%;
+    display: inline-block;
+    overflow: hidden;
+    height: 200px;
+    width: 200px;
+  }
+
+  .img img {
+    height: 100%;
+  }
+
+  .card-committee {
+    padding: 30px;
+    float: none;
+    margin: 0 auto;
+  }
+
+  .card-committee small {
+    font-size: 120%;
+  }
+
+  .card-committee small, .card-committee h3 {
+    font-weight: 600;
+  }
+
+  #committee {
+    padding-bottom: 30px;
+  }
+</style>
