96d7e18fb3b77ded32682ffb71516d546bd838dc
Benjapol Worakan <benwrk@live.com>
2018-08-17 03:18:12 +0700

    Announcement cleanup

--- src/components/Facelift/Announcements.vue
+++ src/components/Facelift/Announcements.vue
@@ -4,52 +4,35 @@
       <div class="row">
         <div class="col-lg-12 mx-auto">
           <h2>ANNOUNCEMENTS</h2>
-          <p class="lead">There are yet to be any announcements</p>
-          <!-- <ul>
-            <li>Clickable nav links that smooth scroll to page sections</li>
-            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
-          </ul> -->
-           <div class="col-md-12 block" v-for="announcement in announcements" v-bind:key="announcement.title">
+            <p class="lead">There are yet to be any announcements</p>
+            <div class="col-md-12 block" v-for="announcement in announcements" v-bind:key="announcement.title">
               <h2 class="title">
-                  <i class="fa fa-star"></i>
-                  {{announcement.title}}
+                <i class="fa fa-star"></i>
+                {{announcement.title}}
               </h2>
               <hr>
-              <div v-html="announcement.body">
-            </div>
-        </div>
+            <div v-html="announcement.body"></div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 <script>
-import carousel from '@/components/Frist/Carousel'
-import timeline from '@/components/Frist/Timeline'
 export default {
   data () {
     return {
       announcements: [
         {
-          title: 'Announcement',
-          body: 'Allap'
-        },
-        {
-          title: 'Announcementa',
-          body: 'Allap'
+          title: 'There are yet to be any announcements',
+          body: 'Allap',
+          time: 'N/A'
         }
       ]
     }
-  },
-  components: {
-    carousel,
-    timeline
   }
 }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
   
 </style>
