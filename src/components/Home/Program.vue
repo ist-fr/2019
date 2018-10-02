00f9a781e956f7752d28a920157316813aec94a4
Norawit Urailertprasert <norawit543@hotmail.com>
2018-10-02 15:23:30 +0700

    update program

--- src/components/Home/Program.vue
+++ src/components/Home/Program.vue
@@ -5,6 +5,84 @@
         <div class="col-lg-12 mx-auto">
           <h2>PROGRAM</h2>
           <p class="lead">The program details for IST-FR 2018 will be released soon</p>
+          <table class="table">
+            <tr>
+                <th>Time</th>
+                <th>Event</th>
+              </tr>
+              <tr>
+                <td>6:30-09:00</td>
+                <td>Bus Departure From BKK To VISTEC</td>
+              </tr>
+              <tr>
+                <td>09:00-09:30</td>
+                <td>Register</td>
+              </tr>
+              <tr>
+                <td>09:30-10:10</td>
+                <td>Keynote Speaker</td>
+              </tr>
+              <tr>
+                <td>10:10-10:25</td>
+                <td>Break</td>
+              </tr>
+              <tr>
+                <td>10:25-10:55</td>
+                <td>Speaker</td>
+              </tr>
+              <tr>
+                <td>10:55-11:25</td>
+                <td>Speaker</td>
+              </tr>
+              <tr>
+                <td>11:25-11:55</td>
+                <td>Speaker</td>
+              </tr>
+              <tr>
+                <td>11:55-12:25</td>
+                <td>Speaker</td>
+              </tr>
+              <tr>
+                <td>12:25-13:30</td>
+                <td>Launch</td>
+              </tr>
+              <tr>
+                <td>13:30-14:00</td>
+                <td>Industry Talks</td>
+              </tr>
+              <tr>
+                <td>14:00-14:30</td>
+                <td>Industry Talks</td>
+              </tr>
+              <tr>
+                <td>14:30-15:00</td>
+                <td>Industry Talks</td>
+              </tr>
+              <tr>
+                <td>15:00-15:15</td>
+                <td>Break</td>
+              </tr>
+              <tr>
+                <td>15:15-16:15</td>
+                <td>Poster Presentation</td>
+              </tr>
+              <tr>
+                <td>16:15-17:15</td>
+                <td>Campus tour</td>
+              </tr>
+              <tr>
+                <td>17:15-17:30</td>
+                <td>Poster Session and Poster Award Ceremony</td>
+              </tr>
+              <tr>
+                <td>17:30-18:00</td>
+                <td>Dinner</td>
+              </tr>
+              <tr>
+                <td>18:00<</td>
+                <td>Bus From VISTEC To BKK</td>
+              </tr>
+          </table>
         </div>
       </div>
     </div>
