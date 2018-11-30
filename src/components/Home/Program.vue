1920dc21dae2a9e14e4cbb392f57904f7e47dea4
Benjapol Worakan <benwrk@live.com>
2018-12-01 06:14:57 +0700

    Update program table styling

--- src/components/Home/Program.vue
+++ src/components/Home/Program.vue
@@ -4,91 +4,95 @@
       <div class="row">
         <div class="col-lg-12 mx-auto">
           <h2>PROGRAM</h2>
-          <table class="table">
-            <tr>
-              <th>Time</th>
-              <th>Event</th>
-            </tr>
-            <tr>
-              <td>06&colon;00 - 06&colon;30</td>
-              <td>Bus transport registration at Bangkok pick-up locations</td>
-            </tr>
-            <tr>
-              <td>06&colon;30 - 09&colon;00</td>
-              <td>Bus journey from Bangkok to VISTEC</td>
-            </tr>
-            <tr>
-              <td>09&colon;00 - 09&colon;20</td>
-              <td>Symposium registration</td>
-            </tr>
-            <tr>
-              <td>09&colon;20 - 09&colon;30</td>
-              <td>Opening ceremony</td>
-            </tr>
-            <tr>
-              <td>09&colon;30 - 10&colon;10</td>
-              <td>Keynote session&colon; &quot;What we need to know about research impacts?&quot; by Dr. Dusit Niyato</td>
-            </tr>
-            <tr>
-              <td>10&colon;10 - 10&colon;40</td>
-              <td>Keynote session&colon; &quot;My research experience and work in Germany and Denmark&quot; by Xiaofeng Xiong</td>
-            </tr>
-            <tr>
-              <td>10&colon;40 - 11&colon;10</td>
-              <td>Reacher experience and vision session&colon; &quot;Advances in the Theory of Graph (Network) Algorithms, and a Little Beyond&quot; by Dr. Danupon Nanongkai</td>
-            </tr>
-            <tr>
-              <td>11&colon;10 - 11&colon;40</td>
-              <td>Reacher experience and vision session by Dr. Supasorn Suwajanakorn</td>
-            </tr>
-            <tr>
-              <td>11&colon;40 - 12&colon;40</td>
-              <td>Lunch break</td>
-            </tr>
-            <tr>
-              <td>12&colon;40 - 13&colon;20</td>
-              <td>Academic talk session by Dr. Thanawin Rakthanmanon</td>
-            </tr>
-            <tr>
-              <td>13&colon;20 - 13&colon;50</td>
-              <td>Academic talk session&colon; &quot;Recent advances in kernel methods for model criticism&quot; by Dr. Wittawat Jitkrittum</td>
-            </tr>
-            <tr>
-              <td>13&colon;50 - 14&colon;20</td>
-              <td>Technology translation session by Prachya Boonkwan</td>
-            </tr>
-            <tr>
-              <td>14&colon;20 - 14&colon;50</td>
-              <td>Technology translation session&colon; &quot;Accurate de novo peptide sequencing with deep learning and positional post-processing&quot; by Sira Sriswasdi</td>
-            </tr>
-            <tr>
-              <td>14&colon;50 - 15&colon;05</td>
-              <td>Afternoon break</td>
-            </tr>
-            <tr>
-              <td>15&colon;05 - 15&colon;45</td>
-              <td>Panel discussion</td>
-            </tr>
-            <tr>
-              <td>15&colon;45 - 16&colon;15</td>
-              <td>Poster session</td>
-            </tr>
-            <tr>
-              <td>16&colon;15 - 17&colon;15</td>
-              <td>Campus tour</td>
-            </tr>
-            <tr>
-              <td>17&colon;15 - 17&colon;30</td>
-              <td>Prize giving ceremony</td>
-            </tr>
-            <tr>
-              <td>17&colon;30 - 18&colon;15</td>
-              <td>Dinner</td>
-            </tr>
-            <tr>
-              <td>18&colon;15 - 20&colon;30</td>
-              <td>Bus journey from VISTEC back to Bangkok</td>
-            </tr>
+          <table class="table table-responsive program-table">
+            <thead>
+              <tr>
+                <th scope="col" style="min-width: 8em;">Time</th>
+                <th scope="col">Event</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope="row">06&colon;00 - 06&colon;30</td>
+                <td>Bus transport registration at Bangkok pick-up locations</td>
+              </tr>
+              <tr>
+                <td scope="row">06&colon;30 - 09&colon;00</td>
+                <td>Bus journey from Bangkok to VISTEC</td>
+              </tr>
+              <tr>
+                <td scope="row">09&colon;00 - 09&colon;20</td>
+                <td>Symposium registration</td>
+              </tr>
+              <tr>
+                <td scope="row">09&colon;20 - 09&colon;30</td>
+                <td>Opening ceremony</td>
+              </tr>
+              <tr>
+                <td scope="row">09&colon;30 - 10&colon;10</td>
+                <td>Keynote session&colon; &quot;What we need to know about research impacts?&quot; by Dr. Dusit Niyato</td>
+              </tr>
+              <tr>
+                <td scope="row">10&colon;10 - 10&colon;40</td>
+                <td>Keynote session&colon; &quot;My research experience and work in Germany and Denmark&quot; by Xiaofeng Xiong</td>
+              </tr>
+              <tr>
+                <td scope="row">10&colon;40 - 11&colon;10</td>
+                <td>Reacher experience and vision session&colon; &quot;Advances in the Theory of Graph (Network) Algorithms, and a Little Beyond&quot; by Dr. Danupon Nanongkai</td>
+              </tr>
+              <tr>
+                <td scope="row">11&colon;10 - 11&colon;40</td>
+                <td>Reacher experience and vision session by Dr. Supasorn Suwajanakorn</td>
+              </tr>
+              <tr>
+                <td scope="row">11&colon;40 - 12&colon;40</td>
+                <td>Lunch break</td>
+              </tr>
+              <tr>
+                <td scope="row">12&colon;40 - 13&colon;20</td>
+                <td>Academic talk session by Dr. Thanawin Rakthanmanon</td>
+              </tr>
+              <tr>
+                <td scope="row">13&colon;20 - 13&colon;50</td>
+                <td>Academic talk session&colon; &quot;Recent advances in kernel methods for model criticism&quot; by Dr. Wittawat Jitkrittum</td>
+              </tr>
+              <tr>
+                <td scope="row">13&colon;50 - 14&colon;20</td>
+                <td>Technology translation session by Prachya Boonkwan</td>
+              </tr>
+              <tr>
+                <td scope="row">14&colon;20 - 14&colon;50</td>
+                <td>Technology translation session&colon; &quot;Accurate de novo peptide sequencing with deep learning and positional post-processing&quot; by Sira Sriswasdi</td>
+              </tr>
+              <tr>
+                <td scope="row">14&colon;50 - 15&colon;05</td>
+                <td>Afternoon break</td>
+              </tr>
+              <tr>
+                <td scope="row">15&colon;05 - 15&colon;45</td>
+                <td>Panel discussion</td>
+              </tr>
+              <tr>
+                <td scope="row">15&colon;45 - 16&colon;15</td>
+                <td>Poster session</td>
+              </tr>
+              <tr>
+                <td scope="row">16&colon;15 - 17&colon;15</td>
+                <td>Campus tour</td>
+              </tr>
+              <tr>
+                <td scope="row">17&colon;15 - 17&colon;30</td>
+                <td>Prize giving ceremony</td>
+              </tr>
+              <tr>
+                <td scope="row">17&colon;30 - 18&colon;15</td>
+                <td>Dinner</td>
+              </tr>
+              <tr>
+                <td scope="row">18&colon;15 - 20&colon;30</td>
+                <td>Bus journey from VISTEC back to Bangkok</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
