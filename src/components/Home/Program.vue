4d436fdbd9030e337c1897a1cf2d05987f24fab3
Benjapol Worakan <benwrk@live.com>
2018-12-01 05:40:31 +0700

    Update program

--- src/components/Home/Program.vue
+++ src/components/Home/Program.vue
@@ -10,80 +10,80 @@
                 <th>Event</th>
               </tr>
               <tr>
-                <td>06:00-06:30</td>
-                <td>Bus Register @ PTT Headquarter</td>
+                <td>06:00 - 06:30</td>
+                <td>Bus transport registration at PTT plc headquarters in Bangkok</td>
               </tr>
               <tr>
-                <td>06:30-09:00</td>
-                <td>Bus Departure From BKK To VISTEC</td>
+                <td>06:30 - 09:00</td>
+                <td>Bus journey from Bangkok to VISTEC</td>
               </tr>
               <tr>
-                <td>09:00-09:30</td>
-                <td>Register</td>
+                <td>09:00 - 09:30</td>
+                <td>Symposium registration</td>
               </tr>
               <tr>
-                <td>09:30-10:10</td>
-                <td>Keynote Speaker</td>
+                <td>09:30 - 10:10</td>
+                <td>Speaker keynote session</td>
               </tr>
               <tr>
-                <td>10:10-10:25</td>
-                <td>Break</td>
+                <td>10:10 - 10:25</td>
+                <td>Morning break</td>
               </tr>
               <tr>
-                <td>10:25-10:55</td>
-                <td>Academic Talks</td>
+                <td>10:25 - 10:55</td>
+                <td>Academic talks</td>
               </tr>
               <tr>
-                <td>10:55-11:25</td>
-                <td>Academic Talks</td>
+                <td>10:55 - 11:25</td>
+                <td>Academic talks</td>
               </tr>
               <tr>
-                <td>11:25-11:55</td>
-                <td>Academic Talks</td>
+                <td>11:25 - 11:55</td>
+                <td>Academic talks</td>
               </tr>
               <tr>
-                <td>11:55-12:25</td>
-                <td>Academic Talks</td>
+                <td>11:55 - 12:25</td>
+                <td>Academic talks</td>
               </tr>
               <tr>
-                <td>12:25-13:30</td>
-                <td>Launch</td>
+                <td>12:25 - 13:30</td>
+                <td>Lunch</td>
               </tr>
               <tr>
-                <td>13:30-14:00</td>
-                <td>Industry Talks</td>
+                <td>13:30 - 14:00</td>
+                <td>Industry talks</td>
               </tr>
               <tr>
-                <td>14:00-14:30</td>
-                <td>Industry Talks</td>
+                <td>14:00 - 14:30</td>
+                <td>Industry talks</td>
               </tr>
               <tr>
-                <td>14:30-15:00</td>
-                <td>Industry Talks</td>
+                <td>14:30 - 15:00</td>
+                <td>Industry talks</td>
               </tr>
               <tr>
-                <td>15:00-15:15</td>
-                <td>Break</td>
+                <td>15:00 - 15:15</td>
+                <td>Afternoon break</td>
               </tr>
               <tr>
-                <td>15:15-16:15</td>
-                <td>Poster Presentation</td>
+                <td>15:15 - 16:15</td>
+                <td>Poster presentation</td>
               </tr>
               <tr>
-                <td>16:15-17:15</td>
+                <td>16:15 - 17:15</td>
                 <td>Campus tour</td>
               </tr>
               <tr>
-                <td>17:15-17:30</td>
-                <td>Poster Session and Poster Award Ceremony</td>
+                <td>17:15 - 17:30</td>
+                <td>Poster session and prize giving</td>
               </tr>
               <tr>
-                <td>17:30-18:15</td>
+                <td>17:30 - 18:15</td>
                 <td>Dinner</td>
               </tr>
               <tr>
                 <td>18:15 onwards</td>
-                <td>Bus From VISTEC To BKK</td>
+                <td>Bus journey from VISTEC back to Bangkok</td>
               </tr>
           </table>
         </div>
