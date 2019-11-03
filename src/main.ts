c064d7cb9abd3f86c004cdb3d434134d847dce1e
Benjapol Worakan <benwrk@live.com>
2019-11-03 22:14:16 +0000

    Add vue-scrollactive

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// @ts-ignore
+import vueScrollactive from 'vue-scrollactive';
+
 Vue.config.productionTip = false;
+
+Vue.use(vueScrollactive);
 
 new Vue({
   router,
