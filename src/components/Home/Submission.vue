db30d929e84b7691fdb404ae4ee8067eb1419056
Benjapol Worakan <benwrk@live.com>
2019-11-29 06:15:41 +0700

    Update submission details (poster presentation & fix dates)

--- src/components/Home/Submission.vue
+++ src/components/Home/Submission.vue
@@ -25,15 +25,15 @@
           <h3>Conditions</h3>
           <div class="alert alert-info rounded-0" role="alert">
             <i class="fa fa-info-circle"></i> The submission deadline has been
-            extended to <span class="font-weight-bold">23 November 2019</span>,
+            extended to <span class="font-weight-bold">29 November 2019</span>,
             submit now if you have yet to! Selection result will be announced
             and notified within 5 December 2019.
           </div>
           <ul>
             <li>
               Researches are to be submitted online via the provided form and
-              are accepted until 23:59 of <strike>17 November 2019</strike>
-              <span class="font-weight-bold"> 23 November 2019</span> (Indochina
+              are accepted until 29:59 of <strike>17 November 2019</strike>
+              <span class="font-weight-bold"> 29 November 2019</span> (Indochina
               time)
             </li>
             <li>
@@ -52,13 +52,42 @@
             </li>
           </ul>
           <h3>Presentation &amp; competition</h3>
-          <div class="alert alert-info rounded-0" role="alert">
+          <!-- <div class="alert alert-info rounded-0" role="alert">
             <i class="fa fa-info-circle"></i> More details about research
             presentation and competition will be announced soon, stay tuned!
-          </div>
+          </div> -->
+          <p>
+            Once your research has been accepted to the symposium, you will have
+            to perform a 30-minute poster presentation session on the symposium day to let
+            the audience know about your work. Our panel of judges will also be
+            evaluating this presentation. Prizes will be given to the researches
+            that our judges believe are most significant and at the front of the
+            frontier. The quality of your presentation also matters a lot since
+            it will help our judges understand your research.
+          </p>
+          <p class="mb-0">
+            Listed below are important details about the poster presentation
+          </p>
+          <ul>
+            <li>
+              You <span class="font-weight-bold">DO NOT</span> have to print out
+              a physical poster, though feel free to do so it helps with your
+              presentation
+            </li>
+            <li>
+              You will be provided with one 32-inch television or better and a
+              HDMI cable
+            </li>
+            <li>
+              It is best to bring your own portable computer for the poster
+              presentation
+            </li>
+          </ul>
           <h3>Criteria</h3>
-          To be eligible for submission, your work must meet all the criteria
-          set out below
+          <p class="mb-0">
+            To be eligible for submission, your work must meet all the criteria
+            set out below
+          </p>
           <ul>
             <li>
               Undergraduate or graduate research in Science or Engineering
@@ -87,6 +116,11 @@
               {{ area }}
             </li>
           </ul>
+          <h3>Prizes</h3>
+          <div class="alert alert-info rounded-0" role="alert">
+            <i class="fa fa-info-circle"></i> More details about prizes will be
+            announced soon, stay tuned!
+          </div>
         </div>
         <div class="col-md-12 text-right">
           <h2>
